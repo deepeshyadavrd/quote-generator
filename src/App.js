2fdd837fe3ca86c7aa727269f01830b18b828866
deepeshyadavrd <raoshsbh6@gmail.com>
2024-08-24 12:26:53 +0530

    axios installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import axios from "axios";
 import "./App.css"
 class App extends React.Component{
   state = { advice: ""};
+
+  componentDidMount(){
+    console.log('Component did mount');
+  }
+
   render(){
     return(
       
